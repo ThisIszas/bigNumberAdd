118a929bd22bc1ea36cc4f9245c47db618427d2f
zhengLi <a838502550@gmail.com>
2016-09-02 22:10:02 +0800

    sourceCode
    简单的大数相加

---
+++ 大数相加.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<stdlib.h> 
+#include <string.h>
+#include<stdio.h>
+using namespace std;
+
+string calculate(string a,string b);
+
+int main(){
+	string fn,sn;
+	cout<<"输入第一个数字和第二个数字.\n"; 
+	cin>>fn>>sn;
+	string result;
+	calculate(fn,sn);
+}
+
+string calculate(string a,string b){
+	int temp=0,i=0,sumOfZero=0,minNumberLength,carryBit=0,fn,sn,j=0;
+	string flag,zeros;
+	int result[256];
+	if(a.length() > b.length())
+	{
+		minNumberLength = b.length();
+		flag = "b";
+	} 
+	else{
+		minNumberLength = a.length();
+		flag = "a";
+	}
+	
+	if(flag=="a"){
+		sumOfZero = b.length() - a.length();
+		for(i=0;i<sumOfZero;i++){
+			zeros+="0";
+		}
+		a= zeros + a;
+	}
+	else{
+		sumOfZero = a.length() - b.length();
+		for(i=0;i<sumOfZero;i++){
+			zeros+="0";
+		}
+		b= zeros + b;
+	} 
+	
+	for(i=(a.length()-1);i>=0;i--){
+		fn = a[i]-'0';
+		sn = b[i]-'0';
+		temp = fn+sn+carryBit;
+		carryBit = 0;
+		if(temp>9){
+			carryBit = 1;
+			temp = temp - 10; 
+		}
+		result[j] = temp;
+		j++;
+	}
+	temp=0;
+	if(carryBit==1) cout<<1;
+	for(i=j-1;i>=0;i--){
+		if((j-i+2)%3==0 && temp!=0) cout<<",";
+		cout<<result[i];
+		temp++;
+	}
+	cout<<endl;
+	return "1";
+}
+
