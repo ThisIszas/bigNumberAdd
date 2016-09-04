19b20148041abc6d0fd27a4c4280f357c848dfda
zhengLi <a838502550@gmail.com>
2016-09-04 10:12:53 +0800

    精简版
    只支持正整数

--- 大数相乘.cpp
+++ 大数相乘.cpp
@@ -17,8 +17,8 @@ int main(){
 }
 
 string calculate(string a,string b){
-	int temp=0,i=0,j=0,sumOfZero=0,carryBit=0,fn,sn,min,k=0,moveStep=0,jj;
-	string flag,zeros,reverse_Order_Result="",result,changeValue;
+	int temp=0,i=0,j=0,sumOfZero=0,carryBit=0,fn,sn,min,k=0,moveStep=0,count=-1;
+	string flag,zeros,result;
 	
 	if(a.length() > b.length()) flag = "b";//找出a,b字符串中较短的那个. 
 	else flag = "a";
@@ -39,61 +39,26 @@ string calculate(string a,string b){
 		}
 		b= zeros + b;
 	} 
-//	if(a.length() == b.length()){
-//		for(i=0;i<a.length();i++){
-//			fn = a[i]-'0';
-//			sn = b[i]-'0';
-//			if(fn!=sn){
-//				if(fn < sn){
-//					changeValue = a;
-//					a = b ;
-//					b = changeValue ;
-//					flag = "true";
-//				}
-//			}
-//		}
-//	} 
-//	for(i=(a.length()-1);i>=(a.length()-min);i--){
+
 	for(i=(a.length()-1);i>=0;i--){
-		//fn = a[i]-'0';
 		sn = b[i]-'0';                            
-//		temp = fn-sn-carryBit;                    
-//		carryBit = 0;
-//		if(temp<0){                               
-//			carryBit = 1;
-//			temp = temp + 10; 
-//		}
 		for(j=(a.length()-1);j>=0;j--){
 			fn = a[j]-'0';
 			temp = fn*sn+carryBit;
-//			cout<<"开始  "<<"carryBit:"<<carryBit<<" temp:"<<temp<<" fn:"<<fn<<" sn:"<<sn<<endl;
 			carryBit = 0;
 			if(temp>9){
 				carryBit = temp/10;
 				temp=temp%10;
 			}
-//			cout<<"之后  "<<"carryBit:"<<carryBit<<" temp:"<<temp<<" fn:"<<fn<<" sn:"<<sn<<endl;
-//			cout<<"j:"<<j<<" k:"<<k<<" min:"<<min<<" j-k+min:"<<j-k+min<<endl;
 			resultList[j-k+min] = resultList[j-k+min] + temp;
-//			for(jj=0;jj<6;jj++) cout<<resultList[jj]<<" ";
-//			cout<<endl; 
 		}
 		if(carryBit!=0) {
-//			cout<<"j:"<<j<<" k:"<<k<<endl;
 			moveStep++;
 			resultList[j-k+min] = resultList[j-1-k+min] + carryBit;
-////			cout<<"外部:"; 
-//			for(jj=0;jj<4;jj++) cout<<resultList[jj]<<" ";
-//			cout<<endl; 
 		}
 		carryBit=0;
 		k++;
-		
-//		char temp2[256];
-//		itoa(temp,temp2,10);
-//		reverse_Order_Result+=temp2;
 	} 
-//	cout<<endl<<"moveStep:"<<moveStep<<" min:"<<min<<" a.length:"<<a.length()<<endl;
 	
 	for(i=(a.length()+min)-1;i>=(min-moveStep);i--){
 		if(resultList[i]>9){
@@ -103,17 +68,13 @@ string calculate(string a,string b){
 			carryBit = 0;
 		}
 	}
-//	for(jj=0;jj<9;jj++) cout<<resultList[jj]<<" ";
-//	cout<<endl;
-//	cout<<"a:"<<a<<" b:"<<b<<endl;
-	int count=-1;
 	for(j=0;j<a.length()+min;j++){
 		if(resultList[i]!=0){
 			count++;
 			break;
 		}
 	}
-	for(i=0;i<a.length()+min;i++) //cout<<resultList[i]<<" ";
+	for(i=0;i<a.length()+min;i++) 
 	{	
 		
 		if(resultList[i]!=0 && i!=count){
@@ -123,9 +84,6 @@ string calculate(string a,string b){
 			result+=temps;
 		}
 	}
-	//for(i=0;i<4;i++)
-//	if(flag == "true") reverse_Order_Result=reverse_Order_Result+'-';
-//	for(i=reverse_Order_Result.length()-1;i>=0;i--) result+=reverse_Order_Result[i];
 	return result;
 }
 
